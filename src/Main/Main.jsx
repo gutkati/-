9745f987752c2489c5b9e359cb917ca95db84cdb
Ekaterina Shmeleva <gutkati@mail.ru>
2023-05-02 13:58:47 +0400

    add component Main

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import './Main.css'
 
 const Main = () => {
     return (
-        <div>
-            
+        <div className='main'>
+            <div className="main__container">
+                <div className="main__container-text">
+                    <div className='main__circle'></div>
+                    <div className="main__container-title">
+                        <h1 className='main__title'>ИДИВИДУАЛЬНОСТЬ</h1>
+                        <h2 className='main__subtitle'>В ТВОИХ РУКАХ!</h2>
+                        <p className='main-text'>Coccinelle более 40 лет производит сумки и аксессуары класса премиум,
+                            являясь синонимом утончённого стиля и итальянского дизайна. Бренд быстро
+                            стал популярным, благодаря изысканному дизайну, качественной натуральной
+                            коже и вниманию к деталям.
+                        </p>
+                    </div>
+
+                        <button className='main__button'>ВСЕ ТОВАРЫ COCCINELLE</button>
+
+
+                </div>
+                <div className="main__container-img">
+                </div>
+            </div>
+
         </div>
     );
 };
